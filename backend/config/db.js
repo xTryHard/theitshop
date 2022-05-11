456bb9aa1b6d561ae4776fd46786a17f581f2a47
Rafa <joser210507@gmail.com>
2022-05-10 23:10:00 -0400

    Update MongoDB config

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,11 +2,7 @@ import mongoose from 'mongoose'
 
 const connectDB = async () => {
   try {
-    const connection = await mongoose.connect(process.env.MONGO_URI, {
-      useUnifiedTopology: true,
-      useNewUrlParser: true,
-      useCreateIndex: true
-    });
+    const connection = await mongoose.connect(process.env.MONGO_URI);
 
     console.log(`MongoDB Connected: ${connection.connection.host}`);
   } catch (error) {
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import connectDB from '../config/db.js'
+import connectDB from './config/db.js'
 import products from './data/products.js';
 
 dotenv.config();
+
+connectDB();
 
 const app = express();
 
