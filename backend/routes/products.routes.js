bb9c2c3a4e5d6b6ae4c0ab509a9b045f27e3db1a
Rafa <joser210507@gmail.com>
2022-05-28 00:14:02 -0400

    Create routes directory

---
+++ backend/routes/products.routes.js
@@ -0,0 +1,13 @@
+import express from 'express';
+const router = express.Router();
+import Product from '../models/productModel.js';
+
+router.get('/', (req, res) => {
+
+});
+
+router.get('/:id', (req, res) => {
+
+});
+
+export default router;
