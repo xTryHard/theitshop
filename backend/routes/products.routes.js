5a0c72db1c66fc6ced88377dc705a713eef5e260
Rafa <joser210507@gmail.com>
2022-05-28 00:32:00 -0400

    Add product routes

--- backend/routes/products.routes.js
+++ backend/routes/products.routes.js
@@ -1,13 +1,30 @@
 import express from 'express';
+import asyncHandler from 'express-async-handler';
 const router = express.Router();
 import Product from '../models/productModel.js';
 
-router.get('/', (req, res) => {
+// @desc    Fetch all products
+// @route   GET /api/products
+// @access  Public
+router.get('/', asyncHandler(async (req, res) => {
+  const products = await Product.find({});
 
-});
+  res.json(products);
+}));
 
-router.get('/:id', (req, res) => {
+// @desc    Fetch single product 
+// @route   GET /api/product/:id
+// @access  Public
+router.get('/:id', asyncHandler(async (req, res) => {
+  const product = await Product.findById(req.params.id);
 
-});
+  if (product) {
+    res.json(product);
+  } else {
+    res.json({
+      message: 'Product not found'
+    }).status(404);
+  }
+}));
 
 export default router;
